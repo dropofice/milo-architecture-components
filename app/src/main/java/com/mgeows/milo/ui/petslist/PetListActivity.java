57722c13e3dbc797f0b81ebb6bdd9be802874513
iceblzzrd <iceblzzrd4@gmail.com>
2017-08-28 07:49:08 +0800

    Add theme and color

--- app/src/main/java/com/mgeows/milo/ui/petslist/PetListActivity.java
+++ app/src/main/java/com/mgeows/milo/ui/petslist/PetListActivity.java
@@ -1,7 +1,9 @@
-package com.mgeows.milo;
+package com.mgeows.milo.ui.petslist;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.mgeows.milo.R;
 
 public class PetListActivity extends AppCompatActivity {
 
